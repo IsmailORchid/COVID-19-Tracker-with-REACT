f5a054cfb9897cc4f635a68f5e7e8e57ec8fd47a
IsmailORchid <io.thecoder@gmail.com>
2020-07-21 10:22:45 +0100

    complete the header section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState, useEffect} from 'react';
+import {MenuItem,FormControl,Select} from '@material-ui/core'
 import './App.css';
 
 function App() {
+  const [conutries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide')
+  //https://disease.sh/v3/covid-19/countries
+  const baseUrl = "https://disease.sh/v3/covid-19/countries";
+  useEffect(()=>{
+    // The code inside here will run once
+    // when the component loads and not again
+     (async function(){
+      // async ->  send a request wait fo it to do something 
+       await fetch(baseUrl)
+             .then(response => response.json())
+             .then(data => {
+               const countries = data.map(country=>(
+                 {
+                   name: country.country,
+                   value:country.countryInfo.iso2
+                 }
+               ));
+               setCountries(countries);
+             })
+     })()
+  },[conutries]);
+  const onCountryyChange = async (event)=>{
+    const countryCode = event.target.value;
+    console.log("Yooooo <<<< ",countryCode);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    
+       <div className="app__header">
+       <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+          variant="outlined"
+          value={country}
+          onChange={onCountryyChange}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+           {conutries.map( (country,i) =>(
+             <MenuItem key={i} value={country.value}>{country.name}</MenuItem>
+           ))}
+          </Select>
+          
+        </FormControl>
+       </div>
+     
+     <div className="app__stats">
+      
+     </div>
+      
+      {/**Table */}
+      {/**Graph */}
+      {/**Mapclear
+       * 
+       */}
     </div>
   );
 }
